24548df2dd87b1f85e66ed43b536b0be78a032e1
chenyn22577 <chenyn22577@hundsun.com>
2020-03-19 10:55:52 +0800

    add:新增java序列化demo演示

--- java-serializbale/src/main/java/com/cyn/bean/People.java
+++ java-serializbale/src/main/java/com/cyn/bean/People.java
@@ -1,5 +1,7 @@
 package com.cyn.bean;
 
+import java.io.*;
+
 /**
  * 文件描述
  *
@@ -10,5 +12,41 @@ package com.cyn.bean;
  * @Version: 1.0
  * @Description: note
  **/
-public class People {
+public class People implements Serializable {
+
+    private static final long serialVersionUID = 3282215350418518214L;
+    //private static final long serialVersionUID = 3282215350418518215L;
+
+    private String name;
+    private int age;
+
+    public static String pre = "沉默";
+    transient String meizi = "王三";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", meizi='" + meizi + '\'' +
+                ", pre='" + pre + '\'' +
+                '}';
+    }
+
 }
--- java-serializbale/src/main/java/com/cyn/bean/TestMain.java
+++ java-serializbale/src/main/java/com/cyn/bean/TestMain.java
@@ -1,5 +1,7 @@
 package com.cyn.bean;
 
+import java.io.*;
+
 /**
  * 文件描述
  *
@@ -11,4 +13,28 @@ package com.cyn.bean;
  * @Description: note
  **/
 public class TestMain {
+    public static void main(String[] args) {
+//        // 初始化
+//        People wanger = new People();
+//        wanger.setName("王二");
+//        wanger.setAge(18);
+//        System.out.println(wanger);
+//
+//        // 把对象写到文件中
+//        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("chenmo"));){
+//            oos.writeObject(wanger);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        // 改变 static 字段的值
+//        People.pre ="不沉默";
+
+        // 从文件中读出对象
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File("chenmo")));){
+            People wanger1 = (People) ois.readObject();
+            System.out.println(wanger1);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
