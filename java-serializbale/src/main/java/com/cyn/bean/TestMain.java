fb7823be5750ce535c273cbf18da0ed163abcf32
chenyn22577 <chenyn22577@hundsun.com>
2020-03-19 10:53:30 +0800

    add:新增java序列化demo演示

---
+++ java-serializbale/src/main/java/com/cyn/bean/TestMain.java
@@ -0,0 +1,14 @@
+package com.cyn.bean;
+
+/**
+ * 文件描述
+ *
+ * @ProjectName: java-serializbale
+ * @Package: com.cyn.bean
+ * @Date 2020/3/19 9:33
+ * @Author: chenyn22577
+ * @Version: 1.0
+ * @Description: note
+ **/
+public class TestMain {
+}
