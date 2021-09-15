3bc80779521637eb4f9b71c7cbf114d10f1a417f
ching <775608598@qq.com>
2021-09-15 10:11:30 +0800

    add:StringDemo

--- java-8newfeature/src/main/java/com/cyn/demo/stringDemo/StringDemo.java
+++ java-8newfeature/src/main/java/com/cyn/demo/stringDemo/StringDemo.java
@@ -1,7 +1,12 @@
 package com.cyn.demo.stringDemo;
 
 import cn.hutool.crypto.digest.MD5;
+import com.alibaba.fastjson.JSONArray;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @Description:
@@ -17,5 +22,11 @@ public class StringDemo {
         MD5 md5 = new MD5();
         String s1 = md5.digestHex(t);
         System.out.println(s);
+    }
+
+    @Test
+    public void testString(){
+        String t = "['10086','10087','10088']";
+        JSONArray jsonArray = JSONArray.parseArray(t);
     }
 }
