04f0c44c374ef5f6dd1488c5ad051b0aa37d37d6
ching <775608598@qq.com>
2021-10-19 14:22:10 +0800

    add:list to string demo

--- java-8newfeature/src/main/java/com/cyn/demo/stringDemo/StringDemo.java
+++ java-8newfeature/src/main/java/com/cyn/demo/stringDemo/StringDemo.java
@@ -3,6 +3,8 @@ package com.cyn.demo.stringDemo;
 import cn.hutool.core.util.RandomUtil;
 import cn.hutool.crypto.digest.MD5;
 import com.alibaba.fastjson.JSONArray;
+import com.cyn.demo.bean.People;
+import com.google.common.base.Joiner;
 import org.junit.Test;
 
 import java.util.*;
@@ -31,6 +33,7 @@ public class StringDemo {
             System.out.println(RandomUtil.randomInt(0, jsonArray.size()));
         }
     }
+
     @Test
     public void testString2() {
         String t = "['847d97862a3c4bc6beebf7cefef01085','1d4d169203984ffc9e9a6984e2371ce6']";
@@ -44,5 +47,33 @@ public class StringDemo {
         String s22 = strings.get(0);
         System.out.println(s22);
         System.out.println(strings);
+    }
+
+    @Test
+    public void testString3() {
+//        String t1 = "847d97862a3c4bc6beebf7cefef01085,1d4d169203984ffc9e9a6984e2371ce6";
+//        // String t1 = "847d97862a3c4bc6beebf7cefef010856";
+//        String[] split = t1.split(",");
+//        List<String> strings = Arrays.asList(t1);
+//        String s22 = strings.get(0);
+//        System.out.println(s22);
+//        System.out.println(strings);
+        List<String> strings = new ArrayList<>();
+        strings.add("847d97862a3c4bc6beebf7cefef01085");
+        strings.add("1d4d169203984ffc9e9a6984e2371ce6");
+        System.out.println(strings.toString());
+        People p = new People("1", 1);
+        System.out.println(p.getAge() + 1);
+    }
+    @Test
+    public void testString4() {
+        List<String> strings = new ArrayList<>();
+        strings.add("847d97862a3c4bc6beebf7cefef01085");
+        strings.add("1d4d169203984ffc9e9a6984e2371ce6");
+        String join = String.join(",", strings);
+        String join1 = Joiner.on(",").join(strings);
+        System.out.println(join);
+        System.out.println(join1);
+
     }
 }
