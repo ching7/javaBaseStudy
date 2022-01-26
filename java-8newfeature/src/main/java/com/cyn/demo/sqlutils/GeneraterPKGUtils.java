a2798476aaaec58f52892efc45c4fef69754df68
ching <775608598@qq.com>
2022-01-26 15:56:39 +0800

    add:SQL UTILS

--- java-8newfeature/src/main/java/com/cyn/demo/sqlutils/GeneraterPKGUtils.java
+++ java-8newfeature/src/main/java/com/cyn/demo/sqlutils/GeneraterPKGUtils.java
@@ -19,9 +19,69 @@ public class GeneraterPKGUtils {
     public static void main(String[] args) {
 
         //调用函数
-        System.out.println(generaterModifyColType("create_by", "acd_router_log", "varchar(100)"));
+        // System.out.println(generaterModifyColType("create_by", "acd_router_log", "varchar(100)"));
         //生成文件
+        System.out.println(addMySqlTableColumn("create_by1", "cyn", false, "创建时间", "call_record", "varchar(100)"));
+    }
 
+    public static String createMySqlTable(String tableStr, String tableName) {
+/*        create table if not exists `bps_hsobjects` (
+	            `object_name` varchar(32) not null default ' ' comment '对象名称',
+	            `object_id` int not null default 0 comment 'ID号',
+	            `own_base` varchar(32) not null default ' ' comment '所属表空间',
+	            `object_type` char(1) not null default ' ' comment '对象类型',
+	            `object_version` varchar(20) not null default ' ' comment '对象版本',
+	            `service_name` varchar(32) not null default ' ' comment '服务名称',
+	            `create_date` int not null default 0 comment '创建日期',
+	            `create_time` int not null default 0 comment '创建时间',
+                unique index `uk_hsobjects` (`object_name` asc)
+        ) engine = InnoDB  default charset = utf8 collate = utf8_bin comment = '业务流程系统对象表';
+        commit;*/
+        return null;
+    }
+
+    public static String addMySqlTableColumn(String column, String defaultVal, Boolean nullIs, String comment, String tableName, String type) {
+        StringBuffer pkg = new StringBuffer();
+        String nullableStr = nullIs ? "null" : "not null";
+        pkg.append("drop procedure if exists sp_db_mysql;\r\n");
+        pkg.append("delimiter $$\r\n");
+        pkg.append("create procedure sp_db_mysql()\r\n");
+        pkg.append("begin\r\n");
+        pkg.append("    declare v_rowcount int ;\r\n");
+        pkg.append("    set @v_rowcount = 0;\r\n");
+        pkg.append("    select count(1) into v_rowcount from information_schema.columns where table_name = '" + tableName + "' and column_name = '" + column + "';\r\n");
+        pkg.append("    if v_rowcount=0 then\r\n");
+        pkg.append("        alter table " + tableName + " add " + column + " " + type + " default '" + defaultVal + "' " + nullableStr + " comment '" + comment + "';\r\n ");
+        pkg.append("    end if;\r\n");
+        pkg.append("end $$\r\n");
+        pkg.append("delimiter ;\r\n");
+        pkg.append("call sp_db_mysql();\r\n");
+        pkg.append("drop procedure if exists sp_db_mysql;\r\n");
+        pkg.append("commit;\r\n");
+        return pkg.toString().toLowerCase();
+    }
+
+    /**
+     * 新建表
+     *
+     * @param tableStr  the table str
+     * @param tableName the table name
+     * @return the string
+     */
+    public static String generateCreateTable(String tableStr, String tableName) {
+        StringBuffer pkg = new StringBuffer();
+        pkg.append("declare\r\n");
+        pkg.append("    v_table varchar2(100):='" + tableName + "';\r\n");
+        pkg.append("    v_sql varchar2(2000):='" + tableStr.trim() + "';\r\n");
+        pkg.append("    v_cnt number:=0;\r\n");
+        pkg.append("begin\r\n");
+        pkg.append("    select count(1) into v_cnt from user_tables t where t.TABLE_NAME=v_table ;\r\n");
+        pkg.append("    if v_cnt=0 then\r\n");
+        pkg.append("       execute immediate v_sql;\r\n");
+        pkg.append("    end if;\r\n");
+        pkg.append("end;\r\n");
+        pkg.append("/\r\n");
+        return pkg.toString().toUpperCase();
     }
 
     public static String generaterModifyColType(String column, String tableName, String type) {
@@ -59,22 +119,6 @@ public class GeneraterPKGUtils {
         return pkg.toString().toUpperCase();
     }
 
-    public static String generateCreateTable(String tableStr, String tableName) {
-        StringBuffer pkg = new StringBuffer();
-        pkg.append("declare\r\n");
-        pkg.append("    v_table varchar2(100):='" + tableName + "';\r\n");
-        pkg.append("    v_sql varchar2(2000):='" + tableStr.trim() + "';\r\n");
-        pkg.append("    v_cnt number:=0;\r\n");
-        pkg.append("begin\r\n");
-        pkg.append("    select count(1) into v_cnt from user_tables t where t.TABLE_NAME=v_table ;\r\n");
-        pkg.append("    if v_cnt=0 then\r\n");
-        pkg.append("       execute immediate v_sql;\r\n");
-        pkg.append("    end if;\r\n");
-        pkg.append("end;\r\n");
-        pkg.append("/\r\n");
-        return pkg.toString().toUpperCase();
-    }
-
     public static String renameColumn(String column, String tableName, String type) {
         StringBuffer pkg = new StringBuffer();
         pkg.append("declare\r\n");
@@ -85,6 +129,23 @@ public class GeneraterPKGUtils {
         pkg.append("begin\r\n");
         pkg.append("    select count(1) into v_cnt from user_tab_columns t where t.TABLE_NAME=v_table and t.COLUMN_NAME=v_column  ;\r\n");
         pkg.append("    if v_cnt>0 then\r\n");
+        pkg.append("       execute immediate v_sql;\r\n");
+        pkg.append("    end if;\r\n");
+        pkg.append("end;\r\n");
+        pkg.append("/\r\n");
+        return pkg.toString().toUpperCase();
+    }
+
+    public static String addColumn(String column, String tableName, String type) {
+        StringBuffer pkg = new StringBuffer();
+        pkg.append("declare\r\n");
+        pkg.append("    v_column varchar2(100):='" + column.trim() + "';\r\n");
+        pkg.append("    v_table varchar2(100):='" + tableName.trim() + "';\r\n");
+        pkg.append("    v_sql varchar2(2000):='ALTER TABLE " + tableName + " ADD " + column + " " + type + "';\r\n");
+        pkg.append("    v_cnt number:=0;\r\n");
+        pkg.append("begin\r\n");
+        pkg.append("    select count(1) into v_cnt from user_tab_columns t where t.TABLE_NAME=v_table and t.COLUMN_NAME=v_column  ;\r\n");
+        pkg.append("    if v_cnt=  0 then\r\n");
         pkg.append("       execute immediate v_sql;\r\n");
         pkg.append("    end if;\r\n");
         pkg.append("end;\r\n");
