abccf31ec948c5372c713494dcccbdc31b09ddb4
ynchen9 <ynchen9@iflytek.com>
2024-07-11 16:13:30 +0800

    update:pom.xml

--- java-collection/src/main/java/com/cyn/demo/HashTest.java
+++ java-collection/src/main/java/com/cyn/demo/HashTest.java
@@ -1,8 +1,6 @@
 package com.cyn.demo;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * @ClassName HashTest
@@ -15,13 +13,20 @@ public class HashTest {
     public static void main(String[] args) {
         int t = "2".hashCode();
         System.out.println(t);
-
-        HashMap<String,List<String>> ht = new HashMap<String, List<String>>();
-        ht.put("1", Arrays.asList("11","12"));
-        ht.put("2", Arrays.asList("21","22"));
-        ht.put("3", Arrays.asList("31","32"));
-        ht.put("4", Arrays.asList("41","42"));
-
-        ht.entrySet();
+        ArrayList<String> objects = new ArrayList<>(null);
+        objects.forEach(System.out::println);
+        HashMap<String, List<String>> ht = new HashMap<String, List<String>>();
+//        ht.put("1", Arrays.asList("11","12"));
+//        ht.put("2", Arrays.asList("21","22"));
+        ht.put("3", Collections.emptyList());
+//        ht.put("4", Arrays.asList("41","42"));
+        List<String> fs = new ArrayList<>();
+        ht.forEach((s, ss) -> {
+            if (!ss.isEmpty()) {
+                fs.addAll(ss);
+            }
+        });
+        boolean empty =  fs.isEmpty();
+        System.out.println(empty);
     }
 }
